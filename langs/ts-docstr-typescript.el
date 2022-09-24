90b65fed236e4179836dba728d5f500d76b68ce4
Jen-Chieh Shen <jcs090218@gmail.com>
2022-09-24 17:45:46 +0800

    done basic typescript

--- langs/ts-docstr-typescript.el
+++ langs/ts-docstr-typescript.el
@@ -88,12 +88,15 @@
     (tsc-node-text node-name)))
 
 ;; NOTE: This is only used in function declaration!
-(defun ts-docstr-typescript--parse-return ()
+(defun ts-docstr-typescript--parse-return (is-method)
   "Return t if function does have return value."
   (when-let*
       ((nodes-fp (ts-docstr-grab-nodes-in-range '(formal_parameters)))
        (node-fp (nth 0 nodes-fp))
        (node-sb (ts-docstr-get-next-sibling node-fp "statement_block")))
+    (when is-method
+      ;; TODO: .. Check if defined in method
+      )
     ;; OKAY: This is probably the best solution!
     ;;
     ;; We traverse the entire tree nad look for `return', if it does return
@@ -122,7 +125,7 @@
                   (ts-docstr-push (tsc-node-text node) variables))))
              param))
           (list :type types :variable variables
-                :return (ts-docstr-typescript--parse-return)
+                :return (ts-docstr-typescript--parse-return is-method)
                 :name (ts-docstr-typescript--get-name node)))
       (list :name (ts-docstr-typescript--get-name node)))))
 
@@ -151,7 +154,29 @@
 ;;;###autoload
 (defun ts-docstr-typescript-insert (node data)
   "Insert document string upon NODE and DATA."
-  (ts-docstr-c++-insert node data))
+  (ts-docstr-inserting
+    (cl-case (tsc-node-type node)
+      ((or function_declaration method_definition)  ; For function
+       (when-let* ((types (plist-get data :type))
+                   (variables (plist-get data :variable))
+                   (len (length variables)))
+         (ts-docstr-insert c-start "\n")
+         (ts-docstr-insert c-prefix (ts-docstr-format 'summary) "\n")
+         (setq restore-point (1- (point)))
+         (dotimes (index len)
+           (ts-docstr-insert c-prefix
+                             (ts-docstr-format 'param
+                                               :typename (nth index types)
+                                               :variable (nth index variables))
+                             "\n"))
+         (when (plist-get data :return)
+           (ts-docstr-insert c-prefix (ts-docstr-format 'return) "\n"))
+         (ts-docstr-insert c-end)))
+      (t  ; For the rest of the type, class/struct/enum
+       (ts-docstr-insert c-start "\n")
+       (ts-docstr-insert c-prefix "\n")
+       (setq restore-point (1- (point)))
+       (ts-docstr-insert c-end)))))
 
 (provide 'ts-docstr-typescript)
 ;;; ts-docstr-typescript.el ends here
